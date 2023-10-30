3b1afe3f6ff7ef3a3eb463b5e72f2477ceabb823
Wangstonn <wangston@umich.edu>
2023-10-29 23:11:55 -0400

    Cherry pick commits from UHD 4.2 to 4.5
    Previously, I branched from a stable branch 4.2  of ettus/uhd. We
    recently upgraded to 4.5, but I cant merge due to loads of conflicts.
    Instead, this is branched from master, so when a new release of ettus
    comes out, the merge from upstream shouldnt cause any problems since it
    should be a fast-forward

--- fpga/usrp3/top/x300/vlsi_make.sh
+++ fpga/usrp3/top/x300/vlsi_make.sh
@@ -2,5 +2,5 @@
 #Build using vlsipool tools
 
 module load python/3.8.5
-. ./setupenv.sh --vivado-path /usr/caen/xilinx/vivado-2019.1/Vivado/
+. ./setupenv.sh --vivado-path /usr/caen/xilinx/vivado-2021.1/Vivado/
 make X310_HG
